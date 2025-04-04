02aa12ebaa643a1ad647cc2ec0aff36b73e78e66
stephenw02 <smwatkins02@gmail.com>
2025-04-04 17:43:14 -0400

    Updated and integrated boot pi and manager files

--- manager.py
+++ manager.py
@@ -3,11 +3,24 @@ from supabase_helper import get_broadcasters
 from listener import listen_for_updates
 from switch_position import read_switch
 from light_controller import light_red, light_blue, light_white, light_off
+from pi_setup import handle_config
+import time
 
 prev_light = None
 
-while True:
+is_configured = handle_config()
+
+while is_configured == False:
+    light_white()
+    time.sleep(1)
+    light_red()
+    time.sleep(1)
+
+
+
+while is_configured==True:
     pi_input = read_switch()
+    broadcast_count = get_broadcasters()
 
     if pi_input == 'Broadcasting':
         try:
@@ -21,10 +34,10 @@ while True:
         except:
             print("Error Listening")
 
-    elif pi_input == "Off" and get_broadcasters() == 0:
+    elif pi_input == "Off" and broadcast_count == 0:
         light_off()
 
-    elif pi_input == "Off" and get_broadcasters() > 0:
+    elif pi_input == "Off" and broadcast_count > 0:
         if prev_light == None or prev_light == "off":
             light_white()
             prev_light = "white"
--- pi_setup.py
+++ pi_setup.py
@@ -2,8 +2,9 @@ import json
 import os
 import time
 import subprocess
+import socket
 
-USB_MOUNT_PATH = "/media/pi"
+USB_MOUNT_PATH = "/media/stephenw02"
 
 def find_config_file():
     for root, dirs, files in os.walk(USB_MOUNT_PATH):
@@ -13,23 +14,26 @@ def find_config_file():
     return None
 
 def apply_wifi_settings(ssid, password):
-    wpa_supplicant_path = "/etc/wpa_supplicant/wpa_supplicant.conf"
-    new_config = f'''
-network={{
-    ssid="{ssid}"
-    psk="{password}"
-}}
-'''
-    with open(wpa_supplicant_path, "a") as f:
-        f.write(new_config)
-    print("📶 WiFi config updated. Rebooting to apply settings...")
-    os.system("sudo reboot")
+    try:
+        socket.create_connection(("8.8.8.8", 53), timeout=3)
+        connected=True
+    except:
+        connected=False
+    if connected==True:
+        return True
+    else:
+        wpa_supplicant_path = "/etc/wpa_supplicant/wpa_supplicant.conf"
+        new_config = f'''network={{ssid="{ssid}"psk="{password}"}}'''
+        with open(wpa_supplicant_path, "a") as f:
+            f.write(new_config)
+        print("📶 WiFi config updated. Rebooting to apply settings...")
+        os.system("sudo reboot")
 
 def configure_raspotify(username, password):
     config_path = "/etc/raspotify/conf"
     if not os.path.exists(config_path):
         print("❌ Raspotify config not found. Is it installed?")
-        return
+        return False
 
     with open(config_path, "r") as f:
         lines = f.readlines()
@@ -57,21 +61,27 @@ def configure_raspotify(username, password):
 
     print("🎵 Raspotify credentials updated. Restarting service...")
     subprocess.run(["sudo", "systemctl", "restart", "raspotify"])
+    return True
 
 def handle_config():
     config_file = find_config_file()
     if not config_file:
         print("❌ No config.json file found on USB.")
-        return
+        return False
 
     with open(config_file, "r") as f:
         data = json.load(f)
 
     if "wifi_ssid" in data and "wifi_password" in data:
-        apply_wifi_settings(data["wifi_ssid"], data["wifi_password"])
+        wifi_set = apply_wifi_settings(data["wifi_ssid"], data["wifi_password"])
 
     if "spotify_username" in data and "spotify_password" in data:
-        configure_raspotify(data["spotify_username"], data["spotify_password"])
+        raspotify_set = configure_raspotify(data["spotify_username"], data["spotify_password"])
+
+    if raspotify_set==True and wifi_set==True:
+        return True
+    else:
+        return False
 
 if __name__ == "__main__":
     time.sleep(5)  # Give time for USB to mount
